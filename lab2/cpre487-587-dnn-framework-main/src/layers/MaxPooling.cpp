dcb0c5f8f0a30bf3ac63a8a7be982db0aad0b6b3
tjfriedl <tjfriedl@cpre587-f23-10.ece.iastate.edu>
2023-09-18 14:49:00 -0500

    SoftMax is done(hopefully)

--- lab2/cpre487-587-dnn-framework-main/src/layers/MaxPooling.cpp
+++ lab2/cpre487-587-dnn-framework-main/src/layers/MaxPooling.cpp
@@ -19,15 +19,26 @@ void MaxPoolingLayer::computeNaive(const LayerData& dataIn) const {
     size_t inputWidth = inputParams.dims[0]; // Width of array
     size_t inputHeight = inputParams.dims[1]; // Height of array
 
-    size_t outputWidth = outputParams.dims[0];
-    size_t outputHeight = outputParams.dims[1];
+    //size_t outputWidth = outputParams.dims[0];
+    //size_t outputHeight = outputParams.dims[1];
 
     size_t channels = inputParams.dims[2]; // Number of channels, same for I/O
 
-    for (uint32_t x = 0; x < inputWidth; x+=2) {
-        for (uint32_t y = 0; y < inputHeight; y +=2) {
-            for (uint32_t channelDepth = 0; channelDepth < outputParams; channelDepth++) {
-                
+    for (uint32_t y = 0; y < inputWidth; y+=2) {
+        for (uint32_t x = 0; x < inputHeight; x +=2) {
+            for (uint32_t z = 0; z < channels; z++) {
+
+                int _1 = inData[x][y][z];
+                int _2 = inData[x+1][y][z];
+                int _3 = inData[x][y+1][z];
+                int _4 = inData[x+1][y+1][z];
+                int vals[4] = {_1, _2, _3, _4};
+                int max = INT_MIN;
+                for (int i = 0; i < (int)(sizeof(vals)/sizeof(vals[0])); i++) {
+                    if (vals[i] > max) max = vals[i];
+                }
+
+                outData[x/2][y/2][z] = max;
             }
         }
     }
